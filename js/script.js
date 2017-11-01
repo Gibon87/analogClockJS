67680bbc4824fbd4f1d6506214319390f8aee0aa
Mateusz <mateusz.gibki@gmail.com>
2017-11-01 11:29:50 +0100

    lil modification in code

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ function setDate() {
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
-  const hoursDegrees = ((hours / 12) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) + 90; // obliczanie sekund
+  const minutesDegrees = ((minutes / 60) * 360) + 90; // obliczanie minut
+  const hoursDegrees = ((hours / 12) * 360) + 90; // obliczanie godzin
   secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minsHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
   // those statements eliminate "rewinding" of the clock's hands transition degrees
-  
+
   if(secondsDegrees == 90){
     secondsHand.style.transition = `none`;
   }
