e6fbaafa6006ecf495e2885171ed76ee9942d017
Mateusz <mateusz.gibki@gmail.com>
2017-11-01 20:40:40 +0100

    lil change

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function setDate() {
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
-  const secondsDegrees = ((seconds / 60) * 360) + 90; // countingseconds
+  const secondsDegrees = ((seconds / 60) * 360) + 90; // counting seconds
   const minutesDegrees = ((minutes / 60) * 360) + 90; // counting munutes
   const hoursDegrees = ((hours / 12) * 360) + 90; // counting hours
   secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
